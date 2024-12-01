3a3034322455c764766c6a130457911c507b73a5
[ShubhamNaik] <shubham070601@gmail.com>
2024-12-01 18:25:51 +0530

    custome Exception hander is created

--- src/main/java/com/blog/application/payloads/ApiResponse.java
+++ src/main/java/com/blog/application/payloads/ApiResponse.java
@@ -1,26 +1,21 @@
 package com.blog.application.payloads;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class ApiResponse {
 	
 	private String message;
 	private boolean status;
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public boolean isStatus() {
-		return status;
-	}
-	public void setStatus(boolean status) {
-		this.status = status;
-	}
+	private LocalDate date;
 	
 	public ApiResponse(String message, boolean status) {
 		super();
 		this.message = message;
 		this.status = status;
+		this.date = LocalDate.now();
 	}
 	
 }
